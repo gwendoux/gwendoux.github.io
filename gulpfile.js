542132829b3ed5c16095905114061ba812a78a40
gwendoux <gwenael.doux@cncc.fr>
2014-10-21 22:00:27 +0200

    added gulp plugins
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var csso = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
 
 gulp.task('default', function() {
     console.log("this is a test");
