f1d7094edc5c0cfe94e799bd5b4ca2628b590e4a
gwen <gwendoux@gmail.com>
2015-05-03 22:33:40 +0200

    return json to route api/coffee

--- server.js
+++ server.js
@@ -3,26 +3,15 @@ var path = require('path');
 var logger = require('morgan');
 var config = require('./config');
 var Instagram = require('instagram-node-lib');
+var bodyParser = require('body-parser');
 
 
-function getCoffeeBean() {
-    Instagram.set('client_id', config.Instagram.CLIENT_ID);
-    Instagram.set('client_secret', config.Instagram.CLIENT_SECRET);
-    Instagram.set('access_token', config.Instagram.ACCESS_TOKEN);
-
-    Instagram.users.recent({
-        user_id: config.Instagram.User_ID,
-        complete: function(data){
-            var coffeeBeans = data.filter(function(photo) {
-                return photo.tags.indexOf('coffeeoftheday') > -1;
-            });
-
-            return coffeeBeans;
-        }
-    });
-}
+Instagram.set('client_id', config.Instagram.CLIENT_ID);
+Instagram.set('client_secret', config.Instagram.CLIENT_SECRET);
+Instagram.set('access_token', config.Instagram.ACCESS_TOKEN);
 
 var app = express();
+var jsonParser = bodyParser.json();
 
 app.use(logger('dev'));
 
@@ -30,11 +19,18 @@ app.use(logger('dev'));
 app.set('port', process.env.PORT || 8014);
 app.use(express.static(path.join(__dirname, 'www')));
 
-app.get('/api/coffee', function(req, res) {
-    var mycoffee = getCoffeeBean();
 
-    console.log(mycoffee);
-    res.send('hello coffee');
+app.use('/api/coffee', jsonParser, function (req, res) {
+    Instagram.users.recent({
+        user_id: config.Instagram.User_ID,
+        complete: function(data){
+            var coffeeBeans = data.filter(function(photo) {
+                return photo.tags.indexOf('coffeeoftheday') > -1;
+            });
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(JSON.stringify(coffeeBeans, null, 2));
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
