99515bfd3f3d98fda22bd22de84bde9bb2ae8a40
gwen <gwendoux@gmail.com>
2015-05-03 12:22:57 +0200

    get photos with specific tag

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 var express = require ('express');
 var path = require('path');
 var logger = require('morgan');
+var config = require('./config');
+var Instagram = require('instagram-node-lib');
+
+
+Instagram.set('client_id', config.Instagram.CLIENT_ID);
+Instagram.set('client_secret', config.Instagram.CLIENT_SECRET);
+Instagram.set('access_token', config.Instagram.ACCESS_TOKEN);
+
+
+Instagram.users.recent({
+    user_id: config.Instagram.User_ID,
+    complete: function(data){
+        var coffeeBeans = data.filter(function(photo) {
+            return photo.tags.indexOf('coffeeoftheday') > -1;
+        });
+
+        console.log(coffeeBeans);
+    }
+});
 
 var app = express();
 //var apiRouter = express.Router();
