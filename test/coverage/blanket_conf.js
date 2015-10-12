d7f6299c4d3b46a4b7d048ba4fb8fb94d26b65a4
gwendoux <gwenael.doux@cncc.fr>
2015-10-12 17:13:43 +0200

    modified filename
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- test/coverage/blanket_conf.js
+++ test/coverage/blanket_conf.js
@@ -3,7 +3,7 @@ var src_dir = path.join(__dirname, '..', '..');
 console.log("src_dir:", src_dir);
 
 var patterns = [
-    path.join(src_dir, 'app.js'),
+    path.join(src_dir, 'server.js'),
     path.join(src_dir, 'lib'),
     path.join(src_dir, 'routes')
 ];
