b71be43fb799e13489fcd7fb81d34b5f0e592f82
gwendoux <gwenael.doux@cncc.fr>
2015-10-14 17:20:29 +0200

    fixed missing viewbox
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,8 @@ module.exports = function(grunt) {
             options: {
                 prefix: 'icon-',
                 cleanup: true,
-                cleanupdefs: true
+                cleanupdefs: true,
+                inheritviewbox: true
             },
             index : {
                 files: {
