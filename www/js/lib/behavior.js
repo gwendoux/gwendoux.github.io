a69e1cda8848f7c115d626b9c17aede8afb8d442
gwen <gwendoux@gmail.com>
2016-08-12 12:27:57 +0200

    fixed json call

--- www/js/lib/behavior.js
+++ www/js/lib/behavior.js
@@ -15,55 +15,58 @@ ajax.onload = function(e) {
 };
 
 var coffeeRq = utilities.apiBaseUrl() + '/v1/photos/tag/coffeeoftheday';
-
-fetchJsonp(coffeeRq, function(err, data) {
-    var $instafeed = document.getElementById('instafeed');
-    if(err) {
-        $instafeed.innerHTML = '<div class="alert" data-err="'+err+'">cannot get data from instagram</div>';
-    }
-    var html = [];
-    data.slice(0, 6).forEach(function(val) {
-        html.push('<div class="photo-box">');
-        html.push('<img src="' + val.image.standard + '">');
-        html.push('<div class="description">');
-        html.push(val.image.caption);
-        html.push('<div class="date">' + moment(val.image.date, 'X').fromNow() + '</div>');
-        html.push('</div>');
-        html.push('</div>');
-    });
-    $instafeed.innerHTML = html.join('');
-});
-
 var feedRq = utilities.apiBaseUrl() + '/v1/links/';
+var $instafeed = document.getElementById('instafeed');
+var $pinboardfeed = document.getElementById('pinboardfeed');
+var $moreitems  = document.getElementById('show-more-items');
 
-fetchJsonp(feedRq, function(err, data) {
-    var $pinboardfeed = document.getElementById('pinboardfeed');
-    if(err) {
-        $pinboardfeed.innerHTML = '<div class="alert" data-err="'+err+'">cannot get data from pinboard</div>';
-    }
-    var html = [];
-    data.forEach(function(val) {
-        html.push('<div class="reading-list--items">');
-        html.push('<div class="link-wrap">');
-        html.push('<a class="block-link" href="' + val.href + '">');
-        html.push('<h4>' + val.description + '</h4>');
-        html.push('<p><em>' + val.extended + '</em></p>');
-        html.push('<p class="smaller"><em>saved ' + moment(val.time).fromNow() + '</em></p>');
-        html.push('<p class="smaller">source: ' + utilities.getSource(val.href) + '</p>');
-        html.push('</a>');
-        html.push('</div>');
-        html.push('</div>');
+fetchJsonp(coffeeRq)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        var html = [];
+        json.slice(0, 6).forEach(function(val) {
+            html.push('<div class="photo-box">');
+            html.push('<img src="' + val.image.standard + '">');
+            html.push('<div class="description">');
+            html.push(val.image.caption);
+            html.push('<div class="date">' + moment(val.image.date, 'X').fromNow() + '</div>');
+            html.push('</div>');
+            html.push('</div>');
+        });
+        $instafeed.innerHTML = html.join('');
+    }).catch(function(err) {
+        $instafeed.innerHTML = '<div class="alert" data-err="'+err+'">cannot get data from instagram</div>';
     });
-    html.push('<div class="text-centered pinboard-link">');
-    html.push('<button id="show-more-items">Show more items</button>');
-    html.push('</div>');
-    $pinboardfeed.innerHTML = html.join('');
-});
+
+fetchJsonp(feedRq)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        var html = [];
+        json.forEach(function(val) {
+            html.push('<div class="reading-list--items">');
+            html.push('<div class="link-wrap">');
+            html.push('<a class="block-link" href="' + val.href + '">');
+            html.push('<h4>' + val.description + '</h4>');
+            html.push('<p><em>' + val.extended + '</em></p>');
+            html.push('<p class="smaller"><em>saved ' + moment(val.time).fromNow() + '</em></p>');
+            html.push('<p class="smaller">source: ' + utilities.getSource(val.href) + '</p>');
+            html.push('</a>');
+            html.push('</div>');
+            html.push('</div>');
+        });
+        html.push('<div class="text-centered pinboard-link">');
+        html.push('<button id="show-more-items">Show more items</button>');
+        html.push('</div>');
+        $pinboardfeed.innerHTML = html.join('');
+    }).catch(function(err) {
+        $pinboardfeed.innerHTML = '<div class="alert" data-err="'+err+'">cannot get data from pinboard</div>';
+    });
 
 
-document.getElementById('pinboardfeed').addEventListener('click', function(evt) {
+$instafeed .addEventListener('click', function(evt) {
     if (evt.target && evt.target.matches("#show-more-items")) {
-        var $pinboardfeed = document.getElementById('pinboardfeed');
         var className = 'hide-items';
         if ($pinboardfeed.classList) {
             $pinboardfeed.classList.remove('hide-items');
@@ -71,7 +74,7 @@ document.getElementById('pinboardfeed').addEventListener('click', function(evt) 
             $pinboardfeed.className = $pinboardfeed.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
         }
 
-        var $moreitems  = document.getElementById('show-more-items');
+
         $moreitems.insertAdjacentHTML('beforebegin', '<a href="https://pinboard.in/u:Gwendoux">View all on Pinboard.in</a>');
         $moreitems.parentNode.removeChild($moreitems);
     }
