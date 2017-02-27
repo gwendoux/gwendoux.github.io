74ea1e3a636fb27cb443efa5514a178ffd8839f1
gwen <gwendoux@gmail.com>
2017-02-27 17:39:25 +0100

    updated version script

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 var Version = require("node-version-assets");
 
 var versionInstance = new Version({
-    assets: ['dist/css/main.min.css', 'dist/css/resume.min.css', 'dist/js/script.min.js'],
-    grepFiles: ['dist/index.html', 'dist/resume.html']
+    assets: ['dist/css/main.min.css', 'dist/js/script.min.js'],
+    grepFiles: ['dist/index.html']
 });
 versionInstance.run();
